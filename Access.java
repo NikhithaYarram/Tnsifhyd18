95de7374eb61e5c4dccbcfc0f163ad27bc56b7ec
NikhithaYarram <114939739+NikhithaYarram@users.noreply.github.com>
2024-05-02 22:38:14 +0530

    Add files via upload

---
+++ Access.java
@@ -0,0 +1,28 @@
+package com.si.sample;
+class Normal{
+	String s="Hello";
+	int a=45;
+	static String b="sonu";
+	void display() {
+		System.out.println("brown");
+	}
+	static String display1() {
+		return "blue";
+	}
+}
+
+class Access {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+              Normal n=new Normal();
+              System.out.println(n.a);
+              Normal.display1();
+              n.display();
+              System.out.println(n.s);
+              System.out.println(Normal.b);
+              
+	}
+
+
+}
